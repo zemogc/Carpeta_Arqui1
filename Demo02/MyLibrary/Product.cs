6e2853fe10f12f234fa94237d299652721440f4d
Zemog gomez <zemogmjg@gmail.com>
2024-10-03 12:06:47 -0500

    Video 5 minuto contructor

--- Demo02/MyLibrary/Product.cs
+++ Demo02/MyLibrary/Product.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyLibrary
 {
-    public class Product
+    public class Product(int id, string name, decimal UnitPrice, int UnitsInStock)
     {
-        public int Id;
-        public string Name;
-        public decimal UnitPrice;
-        public int UnitsInStock;
+    {
+        //Field, Campo, variable
+
+        //propiedades
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        //Constructor 
+        public Product(int id, string name, decimal UnitPrice, int UnitsInStock) 
+        {
+            Id = id;
+            Name = name;
+            UnitPrice = UnitPrice;
+            UnitsInStock = UnitsInStock;
+
+        }
+            
     }
 }

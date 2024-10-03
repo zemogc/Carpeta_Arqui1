0ef51cdc519a551a91bf38704d4df946db427405
Zemog gomez <zemogmjg@gmail.com>
2024-10-03 12:00:32 -0500

    Video 5 minuto 23:13, propiedades, clases

---
+++ Demo02/MyLibrary/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Product
+    {
+        public int Id;
+        public string Name;
+        public decimal UnitPrice;
+        public int UnitsInStock;
+    }
+}
